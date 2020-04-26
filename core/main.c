b2c5827f97d8e4aea40f14b2f74e960e7cdf74b4
Adam Lerner <a1lerner1@gmail.com>
2020-04-26 11:11:50 -0400

    first

---
+++ core/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+  /* code */
+  // get order input
+  // look up data in file
+  // send data to ship label api
+  // format data to label
+
+  printf("Enter lookup ID or press n to enter manually.\n");
+
+
+  return 0;
+}
+
+// gcc main.c resource.c -o output
