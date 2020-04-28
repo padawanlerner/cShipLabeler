fc89b8d83ec0cc6ded924bc0ad0d417022bb9d54
padawanlerner <a1lerner1@gmail.com>
2020-04-28 15:11:15 -0400

    Update main.c

--- core/main.c
+++ core/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char const *argv[]) {
   /* code */
@@ -6,11 +7,42 @@ int main(int argc, char const *argv[]) {
   // look up data in file
   // send data to ship label api
   // format data to label
+  unsigned option;
+  struct Order
+  {
+    char lookupID[20];
+    char fullName[35];
+  };
+  
+  char lookupField[20];
 
-  printf("Enter lookup ID or press n to enter manually.\n");
+  option = 0;
+
+  while (option < 4)
+  {
+    printf("Welcome to simple shipper.\n");
+    printf("Options:\n\n");
+    printf("1: Create a label from an order entered manually.\n");
+    printf("2: Create a label from an order you lookup from a data source.\n");
+    printf("3: Create a manifest from all for which labels have been created.\n");
+    printf("4: Exit.\n\n");
+    scanf("%d", &option);
 
 
+    if (option != 4)
+    {
+      /* code */
+      printf("lesgo");
+    }
+  }
+  
+
+  
+  
+  
+  
   return 0;
 }
 
 // gcc main.c resource.c -o output
+// cl main.c resource.c /link /output:output.exe
